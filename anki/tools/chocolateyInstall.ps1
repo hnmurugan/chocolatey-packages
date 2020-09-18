c1e05d060d036a8bb04e046151fc8bdc4ef17574
hnmurugan <hnmurugan@gmail.com>
2020-09-18 20:29:27 +0800

    Update chocolateyInstall.ps1

--- anki/tools/chocolateyInstall.ps1
+++ anki/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$checksum = '36690689bb4869956f801d126c47133a43c328c3b0adee2f8bba45d489e9405b'
-$url = 'https://github.com/ankitects/anki/releases/download/2.1.30/anki-2.1.30-windows.exe'
+$url = 'https://github.com/ankitects/anki/releases/download/2.1.33/anki-2.1.33-windows.exe'
 
 Install-ChocolateyPackage `
 	-PackageName 'anki' `
